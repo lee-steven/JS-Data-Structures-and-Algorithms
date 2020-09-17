f6ac2af5c22fbbf3da99e83b535e57935068e739
Steven Lee <lee.steven@wustl.edu>
2020-09-16 20:20:59 -0700

    Binary Search Iterative method

--- Algorithms/Binary Search/BinarySearch.js
+++ Algorithms/Binary Search/BinarySearch.js
@@ -1,5 +1,5 @@
 const BinarySearchRecursiveKernel = (nums, target, left, right) => {
-    const midpoint = (right - left) / 2 + 1
+    const midpoint = (right - left) / 2
 
     if(right < left) return false; 
 
@@ -18,6 +18,23 @@ const BinarySearchRecursive = (nums, target) => {
     return BinarySearchRecursiveKernel(nums, target, 0, nums.length - 1);
 }
 
+const BinarySearchIterative = (nums, target) => {
+    let left = 0, right = nums.length - 1;
+
+    while(left < right) {
+        const midpoint = (right - left) / 2; 
+       
+        if(nums[midpoint] === target) return true;
+        else if(target > nums[midpoint]) {
+            left = midpoint;
+        } else {
+            right = midpoint;
+        }
+    }
+    return false;
+}
+
 // "Main Method"
 const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-console.log(BinarySearchRecursive(nums, 9));
+console.log(BinarySearchRecursive(nums, 1));
+console.log(BinarySearchIterative(nums, 10))
